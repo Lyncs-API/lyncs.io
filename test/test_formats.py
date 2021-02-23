686a61161062827f60d73ab545b94018a43df2ca
Simone Bacchio <s.bacchio@gmail.com>
2021-02-23 13:04:52 +0200

    Removing test_formats

--- test/test_formats.py
+++
@@ -1,8 +0,0 @@
-from pytest import raises
-from lyncs_io.formats import *
-
-
-def test_register():
-    f0 = formats[0]
-    with raises(ValueError):
-        register(f0.name, f0.modules, f0.extensions)
