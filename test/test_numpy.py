89e94b2e8e0689f8ebc7a1345a484aff26bf050a
Simone Bacchio <s.bacchio@gmail.com>
2021-02-23 13:03:17 +0200

    Adding tests for head in numpy

--- test/test_numpy.py
+++ test/test_numpy.py
@@ -10,6 +10,8 @@ def test_numpy():
         io.save(arr, ftmp)
         assert (arr == io.load(ftmp)).all()
         assert (arr == io.load(ftmp, format="numpy")).all()
+        assert io.load(ftmp).shape == io.head(ftmp)["shape"]
+        assert io.load(ftmp).dtype == io.head(ftmp)["dtype"]
 
         ftmp = tmp + "/foo.txt"
         io.save(arr, ftmp)
@@ -20,3 +22,13 @@ def test_numpy():
         io.save(arr, ftmp)
         assert (arr == io.load(ftmp)["arr_0"]).all()
         assert (arr == io.load(ftmp + "/arr_0", format="numpyz")).all()
+        assert list(io.load(ftmp)) == list(io.head(ftmp))
+        assert io.load(ftmp)["arr_0"].shape == io.head(ftmp)["arr_0"]["shape"]
+        assert io.load(ftmp)["arr_0"].dtype == io.head(ftmp)["arr_0"]["dtype"]
+
+        ftmp = tmp + "/foo.npz/arr"
+        io.save(arr, ftmp)
+        assert (arr == io.load(ftmp)).all()
+        assert (arr == io.load(ftmp, format="numpyz")).all()
+        assert io.load(ftmp).shape == io.head(ftmp)["shape"]
+        assert io.load(ftmp).dtype == io.head(ftmp)["dtype"]
