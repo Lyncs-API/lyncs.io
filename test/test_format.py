e7970b0c6d9907c219df7519f7cda23480a0bc96
Simone Bacchio <s.bacchio@gmail.com>
2021-03-23 09:17:29 +0200

    Revert "added decorators to serial tests so that they will be skipped in case they are executed with mpirun/mpiexec"
    This reverts commit aa9239ef472f71ac7bcb02031c80f7dee9ea5d8d.

--- test/test_format.py
+++ test/test_format.py
@@ -1,16 +1,15 @@
+from pytest import raises
 from lyncs_io.format import Formats
-import pytest
 
 
-@pytest.mark.mpi_skip()
 def test_error():
     formats = Formats()
     formats.register(
         "broken", extensions=["bar"], error=ImportError("Not a real format")
     )
 
-    with pytest.raises(ImportError):
+    with raises(ImportError):
         formats.get_format(format="broken")
 
-    with pytest.raises(ImportError):
+    with raises(ImportError):
         formats.get_format(filename="foo.bar")
--- test/test_hdf5.py
+++ test/test_hdf5.py
@@ -2,10 +2,8 @@ from pathlib import Path
 import lyncs_io as io
 import numpy as np
 import tempfile
-import pytest
 
 
-@pytest.mark.mpi_skip()
 def test_numpy():
     arr = np.random.rand(100)
     with tempfile.TemporaryDirectory() as tmp:
