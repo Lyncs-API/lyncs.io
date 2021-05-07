0f936ed3d697fb7ab3fdb4e6bd29f65ee0686a90
cstyl <cstyl16@gmail.com>
2021-05-07 22:41:03 +0300

    added tests for ensuring that file is properly overwritten during DaskIO save

--- test/Dask/test_daskio.py
+++ test/Dask/test_daskio.py
@@ -1,3 +1,4 @@
+import os
 import numpy
 import dask
 import dask.array as da
@@ -59,3 +60,43 @@ def test_daskio_write(client, tempdir, domain, chunksize, workers):
 
     assert (x_ref == x_out).all()
     assert (x_ref == x_ref_in).all()
+
+
+@workers_loop
+def test_daskio_write_update(client, tempdir, workers):
+
+    ftmp = tempdir + "foo.npy"
+
+    domains = [
+        (10,),
+        (10, 10, 10),
+        (10, 10),
+        (10, 10, 10, 10),
+    ]
+    chunksize = 5
+
+    daskio = DaskIO(ftmp)
+    # write in the same file arrays of varying domains
+    for domain in domains:
+        size = prod(domain)
+        # chunks should have the same length as domain
+        chunks = tuple(chunksize for a in range(len(domain)))
+
+        x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
+        x_ref = numpy.arange(0, size).reshape(domain)
+
+        x_lazy_out = daskio.save(x_lazy)
+        assert isinstance(x_lazy_out, da.Array)
+
+        x_out = x_lazy_out.compute(num_workers=workers)
+        x_ref_in = io.load(ftmp)
+
+        assert (x_ref == x_out).all()
+        assert (x_ref == x_ref_in).all()
+
+        # ensure file size matches the size of the written array
+        header = io.dask_io._build_header_from_dask_array(x_lazy_out)
+        offset = io.dask_io._get_dask_array_header_offset(header)
+        filesz = offset + size * x_out.itemsize
+
+        assert os.stat(ftmp).st_size == filesz
--- test/Dask/test_daskio_numpy.py
+++ test/Dask/test_daskio_numpy.py
@@ -1,3 +1,4 @@
+import os
 import numpy
 import dask
 import dask.array as da
@@ -49,3 +50,42 @@ def test_numpy_daskio_write(client, tempdir, domain, chunksize, workers):
 
     assert (x_ref == x_out).all()
     assert (x_ref == x_ref_in).all()
+
+
+@workers_loop
+def test_numpy_daskio_write_update(client, tempdir, workers):
+
+    ftmp = tempdir + "foo.npy"
+
+    domains = [
+        (10,),
+        (10, 10, 10),
+        (10, 10),
+        (10, 10, 10, 10),
+    ]
+    chunksize = 5
+
+    # write in the same file arrays of varying domains
+    for domain in domains:
+        size = prod(domain)
+        # chunks should have the same length as domain
+        chunks = tuple(chunksize for a in range(len(domain)))
+
+        x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
+        x_lazy_out = io.save(x_lazy, ftmp)
+        assert isinstance(x_lazy_out, da.Array)
+
+        x_out = x_lazy_out.compute(num_workers=workers)
+        x_ref = numpy.arange(0, size).reshape(domain)
+
+        x_ref_in = io.load(ftmp)
+
+        assert (x_ref == x_out).all()
+        assert (x_ref == x_ref_in).all()
+
+        # ensure file size matches the size of the written array
+        header = io.numpy.head(ftmp)
+        offset = header["_offset"]
+        filesz = offset + size * x_out.itemsize
+
+        assert os.stat(ftmp).st_size == filesz
