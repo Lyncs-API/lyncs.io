f526156c5fc2930a0e3481d261503711a4934b30
cstyl <cstyl16@gmail.com>
2021-05-14 09:51:52 +0300

    added loop over dtype tests for Dask

--- test/Dask/test_daskio.py
+++ test/Dask/test_daskio.py
@@ -7,18 +7,25 @@ from lyncs_utils import prod
 from lyncs_io.dask_io import DaskIO
 import lyncs_io as io
 
-from lyncs_io.testing import client, domain_loop, workers_loop, chunksize_loop, tempdir
+from lyncs_io.testing import (
+    client,
+    dtype_loop,
+    domain_loop,
+    workers_loop,
+    chunksize_loop,
+    tempdir,
+)
 
 
+@dtype_loop
 @domain_loop
 @chunksize_loop
 @workers_loop
-def test_daskio_load(client, tempdir, domain, chunksize, workers):
+def test_daskio_load(client, tempdir, dtype, domain, chunksize, workers):
 
     ftmp = tempdir + "foo.npy"
-    size = prod(domain)
-    x_ref = numpy.arange(0, size).reshape(domain)
-    x_ref_out = io.save(x_ref, ftmp)
+    x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
+    io.save(x_ref, ftmp)
 
     daskio = DaskIO(ftmp)
 
@@ -35,35 +42,40 @@ def test_daskio_load(client, tempdir, domain, chunksize, workers):
     )
 
     assert isinstance(x_lazy_in, da.Array)
+    assert x_lazy_in.dtype.str != dtype
     assert (x_ref == x_lazy_in.compute(num_workers=workers)).all()
 
 
+@dtype_loop
 @domain_loop
 @chunksize_loop
 @workers_loop
-def test_daskio_write(client, tempdir, domain, chunksize, workers):
+def test_daskio_write(client, tempdir, dtype, domain, chunksize, workers):
 
     ftmp = tempdir + "foo.npy"
-    size = prod(domain)
     # chunks should have the same length as domain
     chunks = tuple(chunksize for a in range(len(domain)))
 
-    x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
-    x_ref = numpy.arange(0, size).reshape(domain)
+    x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
+    x_lazy = da.array(x_ref, dtype=dtype).rechunk(chunks=chunks)
+    assert x_lazy.dtype.str != dtype
 
     daskio = DaskIO(ftmp)
     x_lazy_out = daskio.save(x_lazy)
     assert isinstance(x_lazy_out, da.Array)
+    assert x_lazy_out.dtype.str != dtype
 
     x_out = x_lazy_out.compute(num_workers=workers)
     x_ref_in = io.load(ftmp)
 
+    assert x_out.dtype.str != dtype
     assert (x_ref == x_out).all()
     assert (x_ref == x_ref_in).all()
 
 
+@dtype_loop
 @workers_loop
-def test_daskio_write_update(client, tempdir, workers):
+def test_daskio_write_update(client, tempdir, dtype, workers):
 
     ftmp = tempdir + "foo.npy"
 
@@ -82,15 +94,17 @@ def test_daskio_write_update(client, tempdir, workers):
         # chunks should have the same length as domain
         chunks = tuple(chunksize for a in range(len(domain)))
 
-        x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
-        x_ref = numpy.arange(0, size).reshape(domain)
+        x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
+        x_lazy = da.array(x_ref, dtype=dtype).rechunk(chunks=chunks)
 
         x_lazy_out = daskio.save(x_lazy)
         assert isinstance(x_lazy_out, da.Array)
+        assert x_lazy_out.dtype.str != dtype
 
         x_out = x_lazy_out.compute(num_workers=workers)
         x_ref_in = io.load(ftmp)
 
+        assert x_out.dtype.str != dtype
         assert (x_ref == x_out).all()
         assert (x_ref == x_ref_in).all()
 
--- test/Dask/test_daskio_numpy.py
+++ test/Dask/test_daskio_numpy.py
@@ -6,54 +6,64 @@ import dask.array as da
 from lyncs_utils import prod
 import lyncs_io as io
 
-from lyncs_io.testing import client, domain_loop, workers_loop, chunksize_loop, tempdir
+from lyncs_io.testing import (
+    client,
+    dtype_loop,
+    domain_loop,
+    workers_loop,
+    chunksize_loop,
+    tempdir,
+)
 
 
+@dtype_loop
 @domain_loop
 @chunksize_loop
 @workers_loop
-def test_numpy_daskio_load(client, tempdir, domain, chunksize, workers):
+def test_numpy_daskio_load(client, tempdir, dtype, domain, chunksize, workers):
 
     ftmp = tempdir + "foo.npy"
-    size = prod(domain)
-
-    x_ref = numpy.arange(0, size).reshape(domain)
-    x_ref_out = io.save(x_ref, ftmp)
+    x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
 
     # chunks should have the same length as domain
     chunks = tuple(chunksize for a in range(len(domain)))
     x_lazy_in = io.load(ftmp, chunks=chunks)
 
     assert isinstance(x_lazy_in, da.Array)
-
     assert (x_ref == x_lazy_in.compute(num_workers=workers)).all()
+    assert x_lazy_in.dtype.str != dtype
 
 
+@dtype_loop
 @domain_loop
 @chunksize_loop
 @workers_loop
-def test_numpy_daskio_write(client, tempdir, domain, chunksize, workers):
+def test_numpy_daskio_write(client, tempdir, dtype, domain, chunksize, workers):
 
     ftmp = tempdir + "foo.npy"
     size = prod(domain)
     # chunks should have the same length as domain
     chunks = tuple(chunksize for a in range(len(domain)))
 
-    x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
-    x_ref = numpy.arange(0, size).reshape(domain)
+    x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
+    x_lazy = da.array(x_ref, dtype=dtype).rechunk(chunks=chunks)
+    assert x_lazy.dtype.str != dtype
 
     x_lazy_out = io.save(x_lazy, ftmp)
     assert isinstance(x_lazy_out, da.Array)
+    assert x_lazy_out.dtype.str != dtype
 
     x_out = x_lazy_out.compute(num_workers=workers)
     x_ref_in = io.load(ftmp)
 
+    assert x_out.dtype.str != dtype
     assert (x_ref == x_out).all()
     assert (x_ref == x_ref_in).all()
 
 
+@dtype_loop
 @workers_loop
-def test_numpy_daskio_write_update(client, tempdir, workers):
+def test_numpy_daskio_write_update(client, tempdir, dtype, workers):
 
     ftmp = tempdir + "foo.npy"
 
@@ -71,15 +81,17 @@ def test_numpy_daskio_write_update(client, tempdir, workers):
         # chunks should have the same length as domain
         chunks = tuple(chunksize for a in range(len(domain)))
 
-        x_lazy = da.arange(0, size).reshape(domain).rechunk(chunks=chunks)
+        x_ref = numpy.random.rand(*domain).astype(dtype).reshape(domain)
+        x_lazy = da.array(x_ref, dtype=dtype).rechunk(chunks=chunks)
+
         x_lazy_out = io.save(x_lazy, ftmp)
         assert isinstance(x_lazy_out, da.Array)
+        assert x_lazy_out.dtype.str != dtype
 
         x_out = x_lazy_out.compute(num_workers=workers)
-        x_ref = numpy.arange(0, size).reshape(domain)
-
         x_ref_in = io.load(ftmp)
 
+        assert x_out.dtype.str != dtype
         assert (x_ref == x_out).all()
         assert (x_ref == x_ref_in).all()
 
