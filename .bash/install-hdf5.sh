d353602da66404e20d99410bc5226500601863ed
cstyl <cstyl16@gmail.com>
2021-06-24 16:07:41 +0300

    fixing workflow issue with parallel hdf5 in tests

--- .bash/install-hdf5.sh
+++ .bash/install-hdf5.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo apt update #Gets access to packages
-sudo apt install libhdf5-mpi-dev #grabs hdf5 for parallel
-h5pcc - showconfig
-export CC=mpicc
-export HDF5_MPI="ON"
-pip install --no-binary=h5py h5py
+sudo apt-get update -y #Gets access to packages
+sudo apt-get install -y libhdf5-mpi-dev #grabs hdf5 for parallel
+h5pcc --showconfig
+# sudo apt install python3-pip
+# sudo apt-get install -y pkg-config
+# export CC=mpicc
+# export HDF5_MPI="ON"
+HDF5_PATH=$(dirname $(which h5pcc))/../lib
+CC=$CC HDF5_MPI="ON" HDF5_DIR=$HDF5_PATH pip install --no-binary=h5py h5py
