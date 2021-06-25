ace78f401cc64b6abb384860d710adb1f4ba81b0
cstyl <cstyl16@gmail.com>
2021-06-25 15:23:35 +0300

    trying workflow with installing parallel hdf5 and h5py prior to lyncs

--- .bash/install-hdf5.sh
+++ .bash/install-hdf5.sh
@@ -5,7 +5,4 @@ sudo apt-get install -y libhdf5-mpi-dev #grabs hdf5 for parallel
 h5pcc --showconfig
 # sudo apt install python3-pip
 # sudo apt-get install -y pkg-config
-# export CC=mpicc
-# export HDF5_MPI="ON"
-HDF5_PATH=$(dirname $(which h5pcc))/../lib
-CC=mpicc HDF5_MPI="ON" HDF5_DIR=$HDF5_PATH pip install --no-binary=h5py h5py
+CC=mpicc HDF5_MPI="ON" pip install --no-binary=h5py h5py
