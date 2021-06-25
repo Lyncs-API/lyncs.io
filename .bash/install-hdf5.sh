67eeff56ffce10fd4ebc323405c83198de57e84c
cstyl <cstyl16@gmail.com>
2021-06-25 16:25:02 +0300

    trying explicitly specifying hdf5 path

--- .bash/install-hdf5.sh
+++ .bash/install-hdf5.sh
@@ -3,8 +3,8 @@
 sudo apt-get update -y #Gets access to packages
 sudo apt-get install -y libhdf5-mpi-dev #grabs hdf5 for parallel
 h5pcc -showconfig
-var1=$( h5pcc -showconfig | grep "Installation point" )
-HDF5_PATH=$( echo $var1 | awk '{split($0,a," "); print a[3]}' )
+# var1=$( h5pcc -showconfig | grep "Installation point" )
+# HDF5_PATH=$( echo $var1 | awk '{split($0,a," "); print a[3]}' )
 # sudo apt install python3-pip
 # sudo apt-get install -y pkg-config
-CC=mpicc HDF5_MPI="ON" HDF5_DIR=$HDF5_PATH pip install --no-binary=h5py h5py
+CC=mpicc HDF5_MPI="ON" HDF5_DIR="/usr/lib/x86_64-linux-gnu" pip install --no-binary=h5py h5py
