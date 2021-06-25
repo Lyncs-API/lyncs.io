f48bdfbab8beb6d1cb08d88a4d1106ae56ef5bde
cstyl <cstyl16@gmail.com>
2021-06-25 15:58:32 +0300

    obtaining hdf5 path from h5pcc config

--- .bash/install-hdf5.sh
+++ .bash/install-hdf5.sh
@@ -2,7 +2,9 @@
 
 sudo apt-get update -y #Gets access to packages
 sudo apt-get install -y libhdf5-mpi-dev #grabs hdf5 for parallel
-h5pcc --showconfig
+h5pcc -showconfig
+var1=$( h5pcc -showconfig | grep "Installation point" )
+HDF5_PATH=$( echo $var1 | awk '{split($0,a," "); print a[3]}' )
 # sudo apt install python3-pip
 # sudo apt-get install -y pkg-config
-CC=mpicc HDF5_MPI="ON" pip install --no-binary=h5py h5py
+CC=mpicc HDF5_MPI="ON" HDF5_DIR=$HDF5_PATH pip install --no-binary=h5py h5py
