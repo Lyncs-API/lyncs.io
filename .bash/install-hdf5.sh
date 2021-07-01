b95003131bdb3971c78bbb19409333e19adecaff
cstyl <cstyl16@gmail.com>
2021-07-01 13:39:39 +0300

    enabling parallel hdf5 testing in workflows

--- .bash/install-hdf5.sh
+++ .bash/install-hdf5.sh
@@ -3,4 +3,7 @@
 sudo apt-get update -y #Gets access to packages
 sudo apt-get install -y libhdf5-mpi-dev #grabs hdf5 for parallel
 h5pcc -showconfig
-CC=mpicc HDF5_MPI="ON" HDF5_DIR="/usr/lib/x86_64-linux-gnu/hdf5/openmpi" pip install --no-binary=h5py h5py
+
+export $LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/openmpi/lib:/usr/lib/x86_64-linux-gnu/hdf5/openmpi/lib"
+export $INCLUDE_PATH="$INCLUDE_PATH:/usr/lib/x86_64-linux-gnu/openmpi/include:/usr/lib/x86_64-linux-gnu/hdf5/openmpi/include"
+# CC=mpicc HDF5_MPI="ON" HDF5_DIR="/usr/lib/x86_64-linux-gnu/hdf5/openmpi" pip install --no-binary=h5py h5py
