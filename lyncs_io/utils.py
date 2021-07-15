4d52d5f9d8b0165c308d8cb185b8ec491592ade2
Simone Bacchio <s.bacchio@gmail.com>
2021-07-15 17:56:14 +0300

    Moving some utils to lyncs_utils

--- lyncs_io/utils.py
+++ lyncs_io/utils.py
@@ -2,9 +2,12 @@
 Function utils
 """
 
-from functools import wraps
+import os
+import struct
+from functools import wraps, partial
 from io import IOBase, FileIO
 from pathlib import Path
+from lyncs_utils.io import *
 
 
 def swap(fnc):
@@ -12,32 +15,6 @@ def swap(fnc):
     return wraps(fnc)(
         lambda fname, data, *args, **kwargs: fnc(data, fname, *args, **kwargs)
     )
-
-
-def open_file(fnc, arg=0, flag="rb"):
-    "Returns a wrapper that opens the file (at position arg) if needed"
-
-    @wraps(fnc)
-    def wrapped(*args, **kwargs):
-        if len(args) <= arg:
-            raise ValueError(f"filename not found at position {arg}")
-        if isinstance(args[arg], IOBase):
-            return fnc(*args, **kwargs)
-        args = list(args)
-        with open(args[arg], flag) as fptr:
-            args[arg] = fptr
-            return fnc(*args, **kwargs)
-
-    return wrapped
-
-
-def to_path(filename):
-    "Returns a Path from the filename"
-    if isinstance(filename, FileIO):
-        filename = filename.name
-    if isinstance(filename, bytes):
-        filename = filename.decode()
-    return Path(filename)
 
 
 def default_names(i=0):
