a07bc6c290aeaf81dae088b14d111e7357cdc62f
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-07-19 13:44:45 +0300

    Allow files to load if extension is omitted When loading a file with no extension, the directory is searched for a file with the same filename but with an extension. If only one is found, load - otherwise, raise exception.

--- lyncs_io/base.py
+++ lyncs_io/base.py
@@ -28,6 +28,26 @@ def load(filename, format=None, **kwargs):
         One of the implemented formats. See documentation for more details.
     """
 
+    if filename:
+        from os import listdir
+        from os.path import dirname, abspath, splitext
+
+        abs_path = abspath(filename) # Absolute path of filename
+        ext = splitext(filename)[1] # Get the extension of the filename
+        parent_dir_path = dirname(abs_path) # Name of filename's parent directory
+
+        if not ext: 
+            dir_files = listdir(parent_dir_path)
+            if filename not in dir_files:
+                # A list with files matching the following pattern: filename.*
+                possible_files = [f for f in dir_files if splitext(f)[0] == filename]
+
+                # If only one such file exists, load that.
+                if len(possible_files) == 1:
+                    filename = possible_files[0]
+                else:
+                    raise Exception(f'More than one {filename}.* exist')
+
     return formats.get_format(format, filename=filename).load(filename, **kwargs)
 
 
