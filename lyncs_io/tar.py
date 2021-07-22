d173d2a29cc3d724a3617f6657fd04727c39e54b
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-07-22 10:15:13 +0300

    Minor improvements of tar.load/save: Takes the tarball name and file as one param

--- lyncs_io/tar.py
+++ lyncs_io/tar.py
@@ -26,21 +26,9 @@ modes = {
     ':': ['.tar'],
 }
 
+
 # Given the tarball name, return the compression mode using modes
-
-
-def get_mode(ext):
-    for key, val in modes.items():
-        if ext in val:
-            return key
-    raise ValueError(f"{ext} is not supported.")
-
-
-def get_extension(path):
-    return splitext(path)[1]
-
-
-def save(arr, tarball_name, filename):
+def save(arr, filename):
     """
     Save data from an array to a file in a tarball.
 
@@ -50,14 +38,14 @@ def save(arr, tarball_name, filename):
     tarball_name: The name of the tarball.
     filename: The name of the file to save the data to.
     """
-
+    pre, tarball_name, leaf = split_filename(filename)
     mode_suffix = get_mode(get_extension(tarball_name))
 
     # create Tar if doesn't exist - append if it does
-    if exists(tarball_name):
-        tar = tarfile.open(tarball_name, "a")
+    if exists(pre + tarball_name):
+        tar = tarfile.open(pre + tarball_name, "a")
     else:
-        tar = tarfile.open(tarball_name, "w" + mode_suffix)
+        tar = tarfile.open(pre + tarball_name, "w" + mode_suffix)
 
     # A temporary directory is created to write a new file in.
     # The new file is then added to the tarball before being deleted.
@@ -66,37 +54,90 @@ def save(arr, tarball_name, filename):
         for elt in arr:
             dat.write(str(elt) + '\n')
         dat.flush()  # flush data before adding the file to the archive
-        tar.add(dat.name, arcname=filename)
+        tar.add(dat.name, arcname=leaf)
         dat.close()
-        print("Success!")
 
     tar.close()
 
 
-def load(tarball_name, filename):
+
+# def load(tarball_name, filename):
+def load(filename):
     """
     Return an array from the data of a file in a tarball.
     """
 
+    pre, tarball_name, leaf = split_filename(filename)
     mode_suffix = get_mode(get_extension(tarball_name))
 
     arr = [] # store data in
 
-    if exists(tarball_name):
-        tar = tarfile.open(tarball_name, "r" + mode_suffix)
+    if exists(pre + tarball_name):
+        tar = tarfile.open(pre + tarball_name, "r" + mode_suffix)
     else:
         raise FileNotFoundError(f"{tarball_name} does not exist.")
 
-    print(tar.getmembers())
-
     with tempfile.TemporaryDirectory() as tmpf:
-        print(tmpf)
-        member = tar.getmember(filename)
+        member = tar.getmember(leaf)
         tar.extract(member, path=tmpf) # extract to a temp location for reading
-        data_file = f"{tmpf}/{filename}"
+        data_file = f"{tmpf}/{leaf}"
         with open(data_file, "r") as dat:
             for line in dat.readlines():
                 line = line.replace('\n','') # remove the newline character
                 arr.append(line)
     tar.close()
     return arr
+
+
+def get_tarball_from_path(path):
+    """
+    Return the name of the first tarball from a path and its parent directory.
+    e.g. /home/user/tarball.tar/data/arr.npy --> /home/user/, tarball.tar/data/arr.npy
+    """
+    
+    path = path.split('/')
+    
+    for i, f in enumerate(path):
+        if '.tar' in f:
+            return '/'.join(path[:i]) + '/' ,'/'.join(path[i:])
+    raise ValueError("No tarball found in the path")
+
+
+def get_mode(ext):
+    """
+    Returns the mode (from modes) with which the tarball should be read/written as
+    """
+    for key, val in modes.items():
+        if ext in val:
+            return key
+    raise ValueError(f"{ext} is not supported.")
+
+
+def get_extension(filename):
+    """
+    Returns the extension from the filename
+    e.g. 
+    """
+    return splitext(filename)[1]
+
+
+def split_filename(filename):
+    """
+    Splits the filename into 3 parts:
+    - The parent directory of the tarball
+    - The name of the tarball
+    - The file to read/write in the tarball
+    """
+    pre, filename = get_tarball_from_path(filename)
+    cont = filename.split('/')
+    tarball_name = cont[0]
+    leaf = '/'.join(cont[1:])
+    return pre, tarball_name, leaf
+
+# Demo:
+# --------------
+# save an array to a location and load the array to x
+# 
+# save([0,0,1,0,1,1,0], '/home/alexandros/tarball.tar/dir1/datafile.txt')
+# x = load('/home/alexandros/tarball.tar/dir1/datafile.txt')
+# print(x)
