37855b92a4dfe8954211e3c3df925a89f2cf7812
Alexandros Angeli <alexandrosangeli@gmail.com>
2021-08-03 18:51:41 +0300

    Some code refactoring and removal for redundant code

--- lyncs_io/tar.py
+++ lyncs_io/tar.py
@@ -5,9 +5,15 @@ Interface for Tar format
 from os.path import exists, splitext, basename
 from .archive import split_filename
 from io import BytesIO
-from .utils import nested_dict, default_to_regular, default_names
 import tarfile
 import tempfile
+from .utils import (format_key,
+                    is_dir,
+                    nested_dict,
+                    default_to_regular,
+                    default_names,
+                    find_member,
+                    get_depth)
 
 
 _all_extensions = [
@@ -25,9 +31,9 @@ _all_extensions = [
 
 # format extension to be used by formats.get_format
 all_extensions = [splitext(ext)[-1][1:]
-                if sum([1 for x in ext if x == '.']) > 1
-                else ext[1:]
-                for ext in _all_extensions]
+                  if sum([1 for x in ext if x == '.']) > 1
+                  else ext[1:]
+                  for ext in _all_extensions]
 
 modes = {
     # .gz, .bz2 and .xz should start with .tar
@@ -42,17 +48,31 @@ modes = {
 # TODO: issues with h5 files
 # TODO: deduce data format from the data argument (for saving with a default key)
 # TODO: change test_tar.test_serial_tar, use ['arr0'] instead (omit ext)
+# TODO: test parallel save/load
+# TODO: clean up redundant code/ make code more readable
 
 
-def save(arr, filename, key=None, **kwargs):
-    from .base import save as b_save
+def _save(arr, tar, key, **kwargs):
+    from .base import save as bsave
     from .formats import formats
+
+    f = BytesIO()
+    bsave(arr, f, format=formats.get_format(filename=basename(key)))
+    size = f.tell()  # get the size of the file object to write in the tarball
+    f.seek(0)
+    tarinfo = tarfile.TarInfo(name=key)
+    tarinfo.size = size
+    tar.addfile(tarinfo, f)
+    tar.close()
+
+
+def save(arr, filename, key=None, comm=None, **kwargs):
     from lyncs_utils.io import IOBase
     from sys import getsizeof
 
     filename, key = split_filename(filename, key)
     mode_suffix = _get_mode(filename)
-
+    kwargs = {"comm": comm, **kwargs}
 
     # create Tar if doesn't exist - append if it does
     if exists(filename):
@@ -62,43 +82,14 @@ def save(arr, filename, key=None, **kwargs):
 
     if not key:
         for name in default_names():
-            if name +'.npy' not in [m.name for m in tar.getmembers()]:
+            if name + '.npy' not in [m.name for m in tar.getmembers()]:
                 key = name + '.npy'
                 break
 
-    f = BytesIO()
-
-    b_save(arr, f, format=formats.get_format(filename=basename(key)))
-    size = f.tell()  # get the size of the file object to write in the tarball
-    f.seek(0)
-    tarinfo = tarfile.TarInfo(name=key)
-    tarinfo.size = size
-    tar.addfile(tarinfo, f)
-    tar.close()
+    _save(arr, tar, key, **kwargs)
 
 
-def _format_key(key):
-    if key:
-        return key if key[-1] == '/' else key + '/'
-    return '/'
-
-
-def _get_depth(path, key):
-    key_depth = sum([1 for char in key if char == '/'])
-    path_depth = sum([1 for char in path if char == '/'])
-    diff = path_depth - key_depth
-    return diff + 1 if key != '/' else diff + 2
-
-
-
-def _is_dir(tar, key):
-    for member in tar.getmembers():
-        if key == '/' or member.name.startswith(key):
-            return True
-    return False
-
-
-def _load_member(tar, member, header_only, as_data=False):
+def _load_member(tar, member, header_only=False, as_data=False, comm=None, **kwargs):
     from .base import head as bhead
     from .base import load as bload
     from .archive import Data
@@ -108,7 +99,8 @@ def _load_member(tar, member, header_only, as_data=False):
     f = BytesIO()
     f.write(tar.extractfile(member).read())
     f.seek(0)
-    header = Header(bhead(f, format=formats.get_format(filename=basename(member.name))))
+    header = Header(bhead(f, format=formats.get_format(
+                        filename=basename(member.name))))
 
     if header_only:
         return header
@@ -118,7 +110,7 @@ def _load_member(tar, member, header_only, as_data=False):
     return Data(header, data) if as_data else data
 
 
-def _load(paths, tar, header_only, **kwargs):
+def _load(paths, tar, **kwargs):
     new_path_dict = nested_dict()
     for path in paths:
         parts = path.split('/')
@@ -126,62 +118,49 @@ def _load(paths, tar, header_only, **kwargs):
             marcher = new_path_dict
             for key in parts[:-1]:
                 marcher = marcher[key]
-            header = _load_member(tar, _find_member(tar, tar.getmember(path).name), header_only=header_only, as_data=True,)
+            header = _load_member(tar, find_member(tar, tar.getmember(path).name),
+                                  as_data=True, **kwargs)
             marcher[parts[-1]] = header
     return default_to_regular(new_path_dict)
 
 
-def _load_dispatch(tar, key, loader, header_only, depth=1, all_data=False, from_head=False):
-    from .base import head as bhead
-    from .base import load as bload
-    from .formats import formats
+def _load_dispatch(tar, key, loader, depth=1, all_data=False, **kwargs):
     from .archive import Archive
 
     # if .h5
     if key and key.split('/')[0].endswith('.h5'):
-        raise NotImplementedError(f"HDF5 file {key} is not supported when inside a tarball")
+        raise NotImplementedError(
+            f"HDF5 file {key} is not supported when inside a tarball")
 
-    if _is_dir(tar, _format_key(key)):
-        key = _format_key(key)
+    if is_dir(tar, format_key(key)):
+        key = format_key(key)
         paths = [member.name for member in tar.getmembers()
-                if ((member.name.startswith(key) or key=='/')
-                and (_get_depth(member.name, key) <= depth or all_data))]
+                if ((member.name.startswith(key) or key == '/')
+                and (get_depth(member.name, key) <= depth or all_data))]
 
         # avoid {dir : {data}}. Return {data} instead if key is given.
-        _dict = _load(paths, tar, header_only)[key[:-1]] if key != '/' else _load(paths, tar, header_only)
+        _dict = _load(paths, tar, **kwargs)[key[:-1]]\
+                    if key != '/'\
+                    else _load(paths, tar, **kwargs)
         return Archive(_dict, loader=loader, path=key)
 
-    return _load_member(tar, _find_member(tar, key), header_only)
+    return _load_member(tar, find_member(tar, key), **kwargs)
 
 
-def load(filename, key=None, header_only=False, **kwargs):
+def load(filename, key=None, chunks=None, comm=None, **kwargs):
     from .archive import Loader
 
     filename, key = split_filename(filename, key)
     mode_suffix = _get_mode(filename)
+    kwargs = {"comm": comm, **kwargs}
     loader = Loader(load, filename, kwargs=kwargs)
 
     with tarfile.open(filename, "r" + mode_suffix) as tar:
-        return _load_dispatch(tar, key, loader, header_only)
+        return _load_dispatch(tar, key, loader, **kwargs)
 
 
-def _find_member(tar, key):
-    if splitext(key)[-1]:
-            member = tar.getmember(key)
-    else:
-        potential_members = [f.name for f in tar.getmembers() if splitext(f.name)[0] == key]
-        num = len(potential_members)
-        if num == 1:
-            member =  tar.getmember(potential_members[0])
-        elif num > 1:
-            raise KeyError(f"Can't omit extension when multiple files with the same name exist: {','.join(potential_members)}")
-        else:
-            raise KeyError(f"No such file: {key}, {key}.*")
-    return member
-
-
-def head(filename, **kwargs):
-    return load(filename, header_only=True)
+def head(*args, **kwargs):
+    return load(*args, header_only=True, **kwargs)
 
 
 def _get_mode(filename):
--- lyncs_io/utils.py
+++ lyncs_io/utils.py
@@ -6,6 +6,7 @@ from functools import wraps
 from pathlib import Path
 from lyncs_utils.io import FileLike
 from collections import defaultdict
+from os.path import splitext
 
 
 def find_file(filename):
@@ -38,6 +39,8 @@ def find_file(filename):
     if len(potential_files) > 1:
         raise ValueError(f"More than one {filename}.* exist")
     raise FileNotFoundError(f"No such file: {filename}, {filename}.*")
+
+
 
 
 def is_dask_array(obj):
@@ -77,3 +80,55 @@ def default_to_regular(d):
     if isinstance(d, defaultdict):
         d = {k: default_to_regular(v) for k, v in d.items()}
     return d
+
+
+"UTILS USED BY tar.py START HERE"
+def find_member(tar, key):
+    """
+    Similar to find_file but with members in a taball.
+    """
+    if splitext(key)[-1]:
+        member = tar.getmember(key)
+    else:
+        potential_members = [f.name for f in tar.getmembers() if splitext(f.name)[
+            0] == key]
+        num = len(potential_members)
+        if num == 1:
+            member = tar.getmember(potential_members[0])
+        elif num > 1:
+            raise KeyError(
+                f"Can't omit extension when multiple files with the same name exist: {','.join(potential_members)}")
+        else:
+            raise KeyError(f"No such file: {key}, {key}.*")
+    return member
+
+
+def get_depth(path, key):
+    """
+    Returns the depth of a key relatively to a path.
+    E.g. /bar1/bar2/bar3/foo.npy is at depth=2 relatively to /bar1/bar2/
+    """
+    key_depth = sum([1 for char in key if char == '/'])
+    path_depth = sum([1 for char in path if char == '/'])
+    diff = path_depth - key_depth
+    return diff + 1 if key != '/' else diff + 2
+
+
+def format_key(key):
+    """
+    Format the key provided for consistency.
+    """
+    if key:
+        return key if key[-1] == '/' else key + '/'
+    return '/'
+
+
+def is_dir(tar, key):
+    """
+    Check whether a member in a tarball is a directory
+    """
+    for member in tar.getmembers():
+        if key == '/' or member.name.startswith(key):
+            return True
+    return False
+"UTILS USED BY tar.py END HERE"
